e9d4fad9217eef589485b15c0520cf06d4e67141
KingOyin1 <anuoyinloye@gmail.com>
2023-10-09 05:37:24 -0700

    C Variables If Else While Updated

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'Print all single digit numbers of base 10'
+ * Description: 'print all single digit numbers of base 10'
  * Return: Always 0
  */
 int main(void)
 {
-	int n
+	int n;
 
 	for (n = 0; n <= 9; n++)
 	{
