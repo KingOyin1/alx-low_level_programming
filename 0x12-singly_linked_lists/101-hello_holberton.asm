dba8772db95483b8d4bdb67e0fa6cafb860c0803
KingOyin1 <anuoyinloye@gmail.com>
2023-11-17 07:37:27 -0800

    C Singly Linked Lists Project

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char msg[] = "Hello, Holberton";
+	printf("%s\n",msg);
+	return (0);
+}
+
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
